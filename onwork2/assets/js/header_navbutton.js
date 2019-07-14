1b83507ec39d56dcbd2a31ee5297942b734518be
Taeyoon <taeyoon0137@gmail.com>
2019-07-14 14:32:43 +0900

    image test

---
+++ onwork2/assets/js/header_navbutton.js
@@ -0,0 +1,11 @@
+window.onload = ()=> {
+  //header color
+
+
+  //nav button
+  document.querySelector(".nav-button-border").addEventListener("click", ()=> {
+  document.querySelector("#header").classList.toggle("nav--active");
+  document.querySelector("#navigation").classList.toggle("nav--active");
+  document.querySelector("#wrapper").classList.toggle("nav--active");
+  })
+}
